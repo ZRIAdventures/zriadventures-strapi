dd686a537ce8b2a90395b6e3cdbb00909eef77bd
niamatmarjan <146735020+ZRIAdventures@users.noreply.github.com>
2023-12-17 03:40:23 +0530

    Fix lifecycle.js

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,33 +1,32 @@
-const fetch = require('node-fetch'); // Make sure to install this package
+import fetch from 'node-fetch'; // Make sure to install this package
 
 module.exports = {
   models: ['paymentStatus'],
 
-  afterUpdate(event) {
+  afterUpdate: async (event) => {
     const { result, params } = event;
 
     if (result.paymentStatus) {
       // Your API call goes here
       // For example, using fetch:
-      fetch('https://zriadventures-dev.vercel.app/api/strapi/update-status', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params),
-      })
-      .then(response => {
+      try {
+        const response = await fetch('https://zriadventures-dev.vercel.app/api/strapi/update-status', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response.json();
-      })
-      .then(data => {
+
+        const data = await response.json();
         console.log(data);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('There was an error!', error);
-      });
+      }
     }
   },
 };
