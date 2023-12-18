222a6d2567cc8eb6e4a6cfb18bf58f125c090818
niamatmarjan <146735020+ZRIAdventures@users.noreply.github.com>
2023-12-18 15:53:11 +0530

    Update lifecycles.js

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,18 +1,25 @@
 module.exports = {
-    async afterUpdate(event) {
-        const { result, params } = event;
-      const axios = require('axios');
-      // replace with your REST API endpoint
-      const url = 'https://zriadventures-dev.vercel.app/api/strapi/update-order'; 
+  async beforeUpdate(data, model) {
+    let { id } = data;
+    let existing = await strapi.query("Order").findOne({ id });
+
+    if (existing && existing.paymentStatus != model.paymentStatus) {
+      // paymentStatus has changed, so we store the new value in model
+      model.newPaymentStatus = model.paymentStatus;
+    }
+  },
+  async afterUpdate(result, params, data) {
+    const axios = require("axios");
+    const url = "https://zriadventures-dev.vercel.app/api/strapi/update-order";
+
+    // Check if newPaymentStatus exists
+    if (data.newPaymentStatus) {
       try {
-        const response = await axios.post(url, result+"+++"+params);
+        const response = await axios.post(url, result);
         console.log(response.data);
       } catch (error) {
         console.error(error);
       }
-    //   if (params.data.paymentStatus) {
-        
-    //   }
-    },
-  };
-  
+    }
+  },
+};
