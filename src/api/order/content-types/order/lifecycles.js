5f11d9843e6937ceecae65b6b88eb5cad96fc25e
niamatmarjan <146735020+ZRIAdventures@users.noreply.github.com>
2023-12-18 18:03:08 +0530

    Update lifecycles.js

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,18 +1,25 @@
+let previousPaymentStatus;
+
 module.exports = {
+  beforeUpdate(event) {
+    const { result } = event;
+    previousPaymentStatus = result.paymentStatus;
+  },
+
   async afterUpdate(event) {
-    const { result, data, params } = event;
-    const axios = require("axios");
-    const url = "https://zriadventures-dev.vercel.app/api/strapi/update-order";
+    const { result } = event;
 
-    console.log("Result: ", result);
-    console.log("Data: ", data);
-    console.log("Params: ", params);
+    if (result.paymentStatus !== previousPaymentStatus) {
+      const axios = require("axios");
+      const url =
+        "https://zriadventures-dev.vercel.app/api/strapi/update-order";
 
-    try {
-      const response = await axios.post(url, result);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
+      try {
+        const response = await axios.post(url, result);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
   },
 };
