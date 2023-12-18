cd77f4eb82a5d65116aa3937f73c8e4e90e94a2e
niamatmarjan <146735020+ZRIAdventures@users.noreply.github.com>
2023-12-18 17:46:27 +0530

    Update lifecycles.js

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,10 +1,12 @@
 module.exports = {
   async afterUpdate(event) {
-    const { result } = event;
+    const { result, data, params } = event;
     const axios = require("axios");
     const url = "https://zriadventures-dev.vercel.app/api/strapi/update-order";
 
-    console.log(result);
+    console.log("Result: ", result);
+    console.log("Data: ", data);
+    console.log("Params: ", params);
 
     try {
       const response = await axios.post(url, result);
